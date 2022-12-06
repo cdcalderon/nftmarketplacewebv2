6305e3da2f74c072a972fd274a72526c23c858e6
Carlos Calderon <cdcalderon@gmail.com>
2022-12-05 21:12:52 -0600

    update nft listing modal

---
+++ src/components/modals/update-listing-modal/index.js
@@ -0,0 +1,124 @@
+import { ethers } from "ethers";
+import { Input, useNotification } from "web3uikit";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import { useWeb3Contract } from "react-moralis";
+import Button from "@ui/button";
+import nftMarketplaceAbi from "../../../../constants/NftMarketplace.json";
+
+export default function UpdateListingModal({
+    nftAddress,
+    tokenId,
+    isVisible,
+    marketplaceAddress,
+    onClose,
+}) {
+    const dispatch = useNotification();
+
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0);
+
+    // change to async
+    const handleUpdateListingSuccess = async (tx) => {
+        await tx.wait(1);
+        dispatch({
+            type: "success",
+            message: "listing updated",
+            title: "Listing updated - please refresh (and move blocks)",
+            position: "topR",
+        });
+        onClose && onClose();
+        setPriceToUpdateListingWith("0");
+    };
+
+    const { runContractFunction: updateListing } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "updateListing",
+        params: {
+            nftAddress,
+            tokenId,
+            newPrice: ethers.utils.parseEther(priceToUpdateListingWith || "0"),
+        },
+    });
+
+    return (
+        <Modal
+            className="rn-popup-modal placebid-modal-wrapper"
+            show={isVisible}
+            onHide={onClose}
+            centered
+        >
+            {isVisible && (
+                <button type="button" className="btn-close" aria-label="Close">
+                    <i className="feather-x" />
+                </button>
+            )}
+            <Modal.Header>
+                <h3 className="modal-title">Update NFT</h3>
+            </Modal.Header>
+            <Modal.Body>
+                <p>You are about to update this NFT</p>
+                <div className="placebid-form-box">
+                    <h5 className="title">Update listing price in (ETH)</h5>
+                    <div className="bid-content">
+                        <div className="bid-content-top">
+                            <div className="bid-content-left">
+                                <input
+                                    id="value"
+                                    type="text"
+                                    name="value"
+                                    onChange={(event) => {
+                                        setPriceToUpdateListingWith(
+                                            event.target.value
+                                        );
+                                    }}
+                                />
+                                <span>wETH</span>
+                            </div>
+                        </div>
+
+                        <div className="bid-content-mid">
+                            <div className="bid-content-left">
+                                {/* <span>Your Balance</span> */}
+                                <span>Service fee</span>
+                                {/* <span>Total bid amount</span> */}
+                            </div>
+                            <div className="bid-content-right">
+                                {/* <span>9578 wETH</span> */}
+                                <span>
+                                    {priceToUpdateListingWith * 0.001} wETH
+                                </span>
+                                {/* <span>9588 wETH</span> */}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bit-continue-button">
+                        <Button
+                            size="medium"
+                            fullwidth
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                updateListing({
+                                    onError: (error) => {
+                                        console.log(error);
+                                    },
+                                    onSuccess: handleUpdateListingSuccess,
+                                });
+                            }}
+                        >
+                            Proceed and update listing
+                        </Button>
+                        <Button
+                            color="primary-alta"
+                            size="medium"
+                            className="mt--10"
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
+}
